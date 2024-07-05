e0635600092b24040ca5130ab7d118283cf89d76
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-05 18:55:35 +0000

    beginning p5js work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+let img;
+let raindrops = [];
+let canvasWidth = 1000;
+let canvasHeight = 500;
+let xPos = 0;
+let yPos = 0;
+
+
+function preload(){
+    img = loadImage('./images/forest.jpg')
+}
+
+function setup(){
+createCanvas(canvasWidth,canvasHeight)
+
+for (let i = 0; i < 200; i++){
+    xPos = random(0, canvasWidth);
+    yPos = random(0, -1 * canvasHeight);
+    raindrops.push({yPos: yPos, xPos: xPos})
+}
+}
+
+function draw(){
+    background(img)
+}
