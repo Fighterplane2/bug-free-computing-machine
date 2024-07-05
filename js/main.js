03413acedf224c3b9ca52679d9226733b70ba01f
Jaeden Waller <146506880+Fighterplane2@users.noreply.github.com>
2024-07-05 21:25:41 +0000

    finalized raindrops

--- js/main.js
+++ js/main.js
@@ -1,25 +1,46 @@
 let img;
-let raindrops = [];
+let pointArray = [];
 let canvasWidth = 1000;
 let canvasHeight = 500;
 let xPos = 0;
 let yPos = 0;
-
+let dropNumber = 400;
 
 function preload(){
-    img = loadImage('./images/forest.jpg')
+    img = loadImage('./images/forest.jpg');
+
 }
 
 function setup(){
-createCanvas(canvasWidth,canvasHeight)
-
-for (let i = 0; i < 200; i++){
+createCanvas(canvasWidth,canvasHeight);
+for (let i = 0; i < dropNumber; i++){
     xPos = random(0, canvasWidth);
     yPos = random(0, -1 * canvasHeight);
-    raindrops.push({yPos: yPos, xPos: xPos})
-}
+    pointArray.push({yPos: yPos, xPos: xPos});
+
+    }
+
 }
 
 function draw(){
-    background(img)
+    background(img);
+    rainDrops();
+
+}
+
+function rainDrops(){
+    for (let point in pointArray){
+        fill(125, 208, 244);
+        noStroke();
+
+        ellipse(pointArray[point].xPos, pointArray[point].yPos, 7, 15);
+        pointArray[point].yPos += 9.796;
+
+        if(pointArray[point].yPos >= canvasHeight){
+            pointArray[point].yPos = 0;
+            pointArray[point].xPos = random(0, canvasWidth);
+        }
+
+    }
+
 }
